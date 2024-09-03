af08b1989a6c6c2c72a4f89f2c96c9be01428a2f
Roshni Syed <105354040+theroshni01@users.noreply.github.com>
2024-09-03 15:55:57 +0530

    Update App.js

--- priceworth/src/App.js
+++ priceworth/src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-      axios.get('http://localhost:3001/user', { withCredentials: true })
+      axios.get(process.env.REACT_APP_URL+'/user', { withCredentials: true })
           .then(response => {
               if (response.data.user) {
                   setIsLoggedIn(true);
