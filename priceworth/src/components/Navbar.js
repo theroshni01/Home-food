3eada2a07807b7459af8c82217a020e7bfaff6e4
Roshni Syed <105354040+theroshni01@users.noreply.github.com>
2024-09-03 16:42:00 +0530

    Update Navbar.js

--- priceworth/src/components/Navbar.js
+++ priceworth/src/components/Navbar.js
@@ -32,22 +32,52 @@ function Navbar({ isLoggedIn, setIsLoggedIn })
 			<a href="#footer">Contact Us</a>
 		</nav>
 	
-		<div class="icons">
+		// <div class="icons">
+		// 	{!isLoggedIn ? (
+				
+		// 		<div class="fas fa-user" id="login-btn"
+		// 	 		><a href="/login">Login</a>
+		// 		</div>
+		// 	):(
+		// 		<div class="fas fa-user" id="login-btn">
+  //           		<button class="logout" onClick={handleLogout}>Logout</button>
+		// 		</div>
+				
+		// 	)
+		// 	}	
+		// </div>
+
+	  	<div className="icons">
+				<div className="profile" id="login-btn">
+			 		<a href="/profile">
+					 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+					<path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+					<path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+					</svg>
+					</a>
+				</div>
+				<div className="profile" id="login-btn">
+			 		<a href="/mycart">
+					 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+					<path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/>
+					</svg>
+					<span class="badge translate-middle rounded-pill bg-dange"> {cartItems.length} </span>
+					</a>
+				</div>
 			{!isLoggedIn ? (
 				
-				<div class="fas fa-user" id="login-btn"
-			 		><a href="/login">Login</a>
+				<div className="login" id="login-btn">
+			 		<a href="/login">Login</a>
 				</div>
 			):(
-				<div class="fas fa-user" id="login-btn">
-            		<button class="logout" onClick={handleLogout}>Logout</button>
+				<div className="fas fa-user" id="login-btn">
+            		<button className="logout" onClick={handleLogout}>Logout</button>
 				</div>
 				
 			)
-			}
-			
-			
-		</div>
+			}	
+
+			</div>
 	</header>
    </div>
   )
