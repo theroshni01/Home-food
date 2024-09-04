ab9ad73258f2c3e4f4209bf75c03d29323b106f9
Roshni Syed <105354040+theroshni01@users.noreply.github.com>
2024-09-04 12:22:17 +0530

    Update Home.js

--- priceworth/src/components/Home.js
+++ priceworth/src/components/Home.js
@@ -18,7 +18,7 @@ function Home() {
     <div>
         <section class="home" id="home">
             <div class="content">
-                <h3>Tasy and fresh <span>Home</span> foods</h3>
+                <h3>Tasty and fresh <span>Home</span> foods</h3>
                 <p>In your busy working days We will provide you a healthy and tasty Home made foods.</p>
                 <a href="#products" class="btn">Subscribe now &rarr;</a>
             </div>
@@ -203,7 +203,7 @@ function Home() {
           <div class="card-body d-flex flex-column">
 	      <h5 class="card-title">Only Breakfast </h5>
               <h5 class="card-title">Weekdays(5 Days) </h5>
-              <p class="card-text">Monthly Subcription for one person</p>
+              <p class="card-text">Monthly Subscription for one person</p>
               <p class="card-text">2000
 	  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-rupee" viewBox="0 0 16 16">
               <path d="M4 3.06h2.726c1.22 0 2.12.575 2.325 1.724H4v1.051h5.051C8.855 7.001 8 7.558 6.788 7.558H4v1.317L8.437 14h2.11L6.095 8.884h.855c2.316-.018 3.465-1.476 3.688-3.049H12V4.784h-1.345c-.08-.778-.357-1.335-.793-1.732H12V2H4z"/>
@@ -218,7 +218,7 @@ function Home() {
           <div class="card-body d-flex flex-column">
 	  <h5 class="card-title">Only Breakfast </h5>
               <h5 class="card-title">Weekend(2 Days)</h5>
-              <p class="card-text">Monthly Subcription for one person</p>
+              <p class="card-text">Monthly Subscription for one person</p>
               <p class="card-text">1000 
 	  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-rupee" viewBox="0 0 16 16">
               <path d="M4 3.06h2.726c1.22 0 2.12.575 2.325 1.724H4v1.051h5.051C8.855 7.001 8 7.558 6.788 7.558H4v1.317L8.437 14h2.11L6.095 8.884h.855c2.316-.018 3.465-1.476 3.688-3.049H12V4.784h-1.345c-.08-.778-.357-1.335-.793-1.732H12V2H4z"/>
@@ -233,7 +233,7 @@ function Home() {
           <div class="card-body d-flex flex-column">
 	  <h5 class="card-title">Only Breakfast </h5>
                 <h5 class="card-title">All Days(7 Days)</h5>
-                <p class="card-text">Monthly Subcription for one person</p>
+                <p class="card-text">Monthly Subscription for one person</p>
                 <p class="card-text">3000 
 	  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-rupee" viewBox="0 0 16 16">
               <path d="M4 3.06h2.726c1.22 0 2.12.575 2.325 1.724H4v1.051h5.051C8.855 7.001 8 7.558 6.788 7.558H4v1.317L8.437 14h2.11L6.095 8.884h.855c2.316-.018 3.465-1.476 3.688-3.049H12V4.784h-1.345c-.08-.778-.357-1.335-.793-1.732H12V2H4z"/>
@@ -253,7 +253,7 @@ function Home() {
           <div class="card-body d-flex flex-column">
 	      <h5 class="card-title">Only Lunch</h5>
               <h5 class="card-title">Weekdays(5 Days) </h5>
-              <p class="card-text">Monthly Subcription for one person</p>
+              <p class="card-text">Monthly Subscription for one person</p>
               <p class="card-text">2500 
 	  	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-rupee" viewBox="0 0 16 16">
 	          <path d="M4 3.06h2.726c1.22 0 2.12.575 2.325 1.724H4v1.051h5.051C8.855 7.001 8 7.558 6.788 7.558H4v1.317L8.437 14h2.11L6.095 8.884h.855c2.316-.018 3.465-1.476 3.688-3.049H12V4.784h-1.345c-.08-.778-.357-1.335-.793-1.732H12V2H4z"/>
@@ -268,7 +268,7 @@ function Home() {
           <div class="card-body d-flex flex-column">
 	  <h5 class="card-title">Only Lunch</h5>
               <h5 class="card-title">Weekend(2 Days)</h5>
-              <p class="card-text">Monthly Subcription for one person</p>
+              <p class="card-text">Monthly Subscription for one person</p>
               <p class="card-text">1250 
 	  	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-rupee" viewBox="0 0 16 16">
               <path d="M4 3.06h2.726c1.22 0 2.12.575 2.325 1.724H4v1.051h5.051C8.855 7.001 8 7.558 6.788 7.558H4v1.317L8.437 14h2.11L6.095 8.884h.855c2.316-.018 3.465-1.476 3.688-3.049H12V4.784h-1.345c-.08-.778-.357-1.335-.793-1.732H12V2H4z"/>
@@ -283,7 +283,7 @@ function Home() {
           <div class="card-body d-flex flex-column">
 	  <h5 class="card-title">Only Lunch</h5>
                 <h5 class="card-title">All Days(7 Days)</h5>
-                <p class="card-text">Monthly Subcription for one person</p>
+                <p class="card-text">Monthly Subscription for one person</p>
                 <p class="card-text">3750 
 	  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-rupee" viewBox="0 0 16 16">
               <path d="M4 3.06h2.726c1.22 0 2.12.575 2.325 1.724H4v1.051h5.051C8.855 7.001 8 7.558 6.788 7.558H4v1.317L8.437 14h2.11L6.095 8.884h.855c2.316-.018 3.465-1.476 3.688-3.049H12V4.784h-1.345c-.08-.778-.357-1.335-.793-1.732H12V2H4z"/>
@@ -302,7 +302,7 @@ function Home() {
           <div class="card-body d-flex flex-column">
 	  <h5 class="card-title">Only Dinner</h5>
               <h5 class="card-title">Weekdays(5 Days) </h5>
-              <p class="card-text">Monthly Subcription for one person</p>
+              <p class="card-text">Monthly Subscription for one person</p>
               <p class="card-text">2000 
 	  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-rupee" viewBox="0 0 16 16">
               <path d="M4 3.06h2.726c1.22 0 2.12.575 2.325 1.724H4v1.051h5.051C8.855 7.001 8 7.558 6.788 7.558H4v1.317L8.437 14h2.11L6.095 8.884h.855c2.316-.018 3.465-1.476 3.688-3.049H12V4.784h-1.345c-.08-.778-.357-1.335-.793-1.732H12V2H4z"/>
@@ -317,7 +317,7 @@ function Home() {
           <div class="card-body d-flex flex-column">
 	   <h5 class="card-title">Only Dinner</h5>
               <h5 class="card-title">Weekend(2 Days)</h5>
-              <p class="card-text">Monthly Subcription for one person</p>
+              <p class="card-text">Monthly Subscription for one person</p>
               <p class="card-text">1000 
 	  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-rupee" viewBox="0 0 16 16">
               <path d="M4 3.06h2.726c1.22 0 2.12.575 2.325 1.724H4v1.051h5.051C8.855 7.001 8 7.558 6.788 7.558H4v1.317L8.437 14h2.11L6.095 8.884h.855c2.316-.018 3.465-1.476 3.688-3.049H12V4.784h-1.345c-.08-.778-.357-1.335-.793-1.732H12V2H4z"/>
@@ -332,7 +332,7 @@ function Home() {
           <div class="card-body d-flex flex-column">
 	   	<h5 class="card-title">Only Dinner</h5>
                 <h5 class="card-title">All Days(7 Days)</h5>
-                <p class="card-text">Monthly Subcription for one person</p>
+                <p class="card-text">Monthly Subscription for one person</p>
                 <p class="card-text">3000 
 	  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-rupee" viewBox="0 0 16 16">
               <path d="M4 3.06h2.726c1.22 0 2.12.575 2.325 1.724H4v1.051h5.051C8.855 7.001 8 7.558 6.788 7.558H4v1.317L8.437 14h2.11L6.095 8.884h.855c2.316-.018 3.465-1.476 3.688-3.049H12V4.784h-1.345c-.08-.778-.357-1.335-.793-1.732H12V2H4z"/>
